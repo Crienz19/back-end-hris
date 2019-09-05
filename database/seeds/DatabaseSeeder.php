d6df0d117c632b712f7ce1b5a3f1fb1e425072c1
Renz Mergenio <zner.mergenio@gmail.com>
2019-09-05 15:14:22 +0800

    Seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,23 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+
+        \App\User::create([
+            'name'          =>  'superadministrator',
+            'email'         =>  'superadministrator@app.com',
+            'password'      =>  bcrypt('password'),
+            'role'          =>  'superadministrator',
+            'isActivated'   =>  1,
+            'isFilled'      =>  1
+        ]);
+
+        \App\User::create([
+            'name'          =>  'humanresource',
+            'email'         =>  'hr@app.com',
+            'password'      =>  bcrypt('password'),
+            'role'          =>  'hr',
+            'isActivated'   =>  1,
+            'isFilled'      =>  1
+        ]);
     }
 }
