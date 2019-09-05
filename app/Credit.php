04df50e51e8493cb02ef62e507946b12ce1f6c8f
Renz Mergenio <zner.mergenio@gmail.com>
2019-09-05 19:09:38 +0800

    Credit Model Fillables

--- app/Credit.php
+++ app/Credit.php
@@ -6,4 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Credit extends Model
 {
+    protected $fillable = [
+        'user_id',
+        'VL',
+        'OT',
+        'OB',
+        'PTO',
+        'total_VL',
+        'total_SL',
+        'total_PTO'
+    ];
 }
