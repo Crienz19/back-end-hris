ce497283cb8cab5e07b5600e2ae1d03789e6ded6
Renz Mergenio <zner.mergenio@gmail.com>
2019-11-12 10:45:32 +0800

    Supervisor Leave notification

--- app/Http/Controllers/Api/Supervisor/LeaveController.php
+++ app/Http/Controllers/Api/Supervisor/LeaveController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api\Supervisor;
 
+use App\Credit;
 use App\Http\Resources\Leave\LeaveResource;
 use App\Http\Resources\Leave\LeaveResourceWithEmployeeAndActions;
 use App\Http\Resources\Leave\LeaveResourceWithEmployeeAndActionsForSup;
@@ -55,6 +56,110 @@ class LeaveController extends Controller
         $dt1 = new \DateTime($request->input('from'));
         $dt2 = new \DateTime($request->input('to'));
         $diff = $dt1->diff($dt2);
+        
+        $data = [
+            'user_id'   =>  auth()->user()->id,
+            'type'      =>  $request->input('type'),
+            'pay_type'  =>  $request->input('pay_type'),
+            'from'      =>  $request->input('from'),
+            'to'        =>  $request->input('to'),
+            'time_from' =>  $request->input('time_from'),
+            'time_to'   =>  $request->input('time_to'),
+            'reason'    =>  $request->input('reason'),
+            'count'     =>  ($request->input('from') == $request->input('to')) ? 1 : $diff->d
+        ];
+
+        $credit = Credit::where('user_id', auth()->user()->id)->first();
+
+        if ($request->input('pay_type') == 'With Pay') {
+            switch ($request->input('type')) {
+                case 'VL':
+                    if ($credit->VL > 0) {
+                        $this->leave->saveLeave($data);
+                        
+                        if ($diff->d > 1) {
+                            return response()->json([
+                                'message'   =>  'Your request has been sent and is waiting to approval. Make sure to inform the Management of you leave request.'
+                            ], 200);
+                        } else {
+                            return response()->json([
+                                'message'   =>  'Leave Submitted!'
+                            ], 200);
+                        }
+                    } else {
+                        return response()->json([
+                            'message'   =>  'You don\'t have Vacation Leave(s)'
+                        ], 401);
+                    }
+                    break;
+
+                case 'SL':
+                    if ($credit->SL > 0) {
+                        $this->leave->saveLeave($data);
+                    } else {
+                        return response()->json([
+                            'message'   =>  'You don\'t have Sick Leave(s)'
+                        ], 401);
+                    }
+                    break;
+
+                case 'PTO':
+                    if ($credit->PTO > 0) {
+                        $this->leave->saveLeave($data);
+
+                        if ($diff->d > 1) {
+                            return response()->json([
+                                'message'   =>  'Your request has been sent and is waiting to approval. Make sure to inform the Management of you leave request.'
+                            ], 200);
+                        } else {
+                            return response()->json([
+                                'message'   =>  'Leave Submitted!'
+                            ], 200);
+                        }
+                    } else {
+                        return response()->json([
+                            'message'   =>  'You don\'t have Personal Time Off(s)'
+                        ], 401);
+                    }
+                    break;
+                case 'VL - Half':
+                    if ($credit->VL > 0) {
+                        $this->leave->saveLeave($data);
+                    } else {
+                        return response()->json([
+                            'message'   =>  'You don\'t have Vacation Leave(s)'
+                        ], 401);
+                    }
+                    break;
+
+                case 'SL - Half':
+                    if ($credit->SL > 0) {
+                        $this->leave->saveLeave($data);
+                    } else {
+                        return response()->json([
+                            'message'   =>  'You don\'t have Sick Leave(s)'
+                        ], 401);
+                    }
+                    break;
+
+                case 'PTO - Half':
+                    if ($credit->PTO > 0) {
+                        $this->leave->saveLeave($data);
+                    } else {
+                        return response()->json([
+                            'message'   =>  'You don\'t have Personal Time Off(s)'
+                        ], 401);
+                    }
+                    break;
+
+            }
+        } else {
+            $this->leave->saveLeave($data);
+
+            return response()->json([
+                'message'   =>  'Leave Submitted!'
+            ], 200);
+        }
 
         if ($diff->d > 3) {
             return response()->json([
@@ -78,7 +183,7 @@ class LeaveController extends Controller
             Notification::route('mail', env('ADMIN_EMAIL'))->notify(new LeaveEmToSupNotification($storedLeave));
 
             return response()->json([
-                'message'   =>  'Leave Added!'
+                'message'   =>  'Your request has been sent and is waiting to approval. Make sure to inform the Management of you leave request.'
             ], 200);
         }
     }
